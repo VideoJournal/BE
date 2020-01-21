b3dc9755e95ecbab12d3a342a833f1b4b1a295ae
Shola Ayeni <ayenisholah@yahoo.com>
2020-01-21 15:35:05 +0100

    created user schema

---
+++ src/resources/user/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    lastName: {
+      type: String,
+      required: false,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8
+    }
+  },
+  { timestamps: true }
+);
+
+
+
+export const User = mongoose.model('user', userSchema);
