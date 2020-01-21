d12c1be99a99c9fbd4e01f6397589a05d782dda8
Shola Ayeni <ayenisholah@yahoo.com>
2020-01-21 15:42:33 +0100

    created a function to verify token

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,3 +5,12 @@ import { User } from '../resources/user/user.model';
 export const newToken = (user) => jwt.sign({ id: user.id }, config.secrets.jwt, {
   expiresIn: config.secrets.jwtExp
 });
+
+export const verifyToken = (token) => {
+  new Promise((resolve, reject) => {
+    jwt.verify(token, config.secrets.jwt, (err, payload) => {
+      if (err) return reject(err);
+      resolve(payload);
+    });
+  });
+};
