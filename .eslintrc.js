9cb6d60c86415cf6884eb1021a43fbcd28d934b5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-21 14:09:29 +0100

    Turned off eslint function paren

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     "no-console": 0,
     "import/prefer-default-export": 0,
     "global-require": 0,
-    "implicit-arrow-linebreak": 0
+    "implicit-arrow-linebreak": 0,
+    "function-paren-newline": 0
   }
 };
