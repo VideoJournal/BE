005005933ce13f7d6c34b2e89f454fb60182cedc
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-21 13:19:35 +0100

    Turned off a eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     "no-unused-expressions": 0,
     "no-console": 0,
     "import/prefer-default-export": 0,
-    "global-require": 0
+    "global-require": 0,
+    "implicit-arrow-linebreak": 0
   }
 };
