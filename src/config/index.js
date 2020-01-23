1119b329335202aadeb4486915f27828cd96d4b2
Damola Adewunmi <damolasd@gmail.com>
2020-01-23 12:16:42 +0100

    changed port to 3000

--- src/config/index.js
+++ src/config/index.js
@@ -8,7 +8,7 @@ const baseConfig = {
   env,
   isDev: env === 'development',
   isTest: env === 'testing',
-  port: 8000,
+  port: 3000,
   secrets: {
     jwt: process.env.JWT_SECRET,
     jwtExp: '100d',
