9936ab86f9766ebdc16f6bd431422dfb240604ab
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-31 11:57:56 +0100

    Turned off eslint operator linebreak rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,30 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true
+    node: true,
   },
-  extends: ["airbnb-base"],
+  extends: ['airbnb-base'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
   },
   parser: 'babel-eslint',
   rules: {
-    "comma-dangle": 0,
-    "consistent-return": 0,
-    "no-underscore-dangle": 0,
-    "func-names": 0,
-    "no-unused-expressions": 0,
-    "no-console": 0,
-    "import/prefer-default-export": 0,
-    "global-require": 0,
-    "implicit-arrow-linebreak": 0,
-    "function-paren-newline": 0,
-    "no-new": 0,
-    "arrow-parens": 0
-  }
+    'comma-dangle': 0,
+    'consistent-return': 0,
+    'no-underscore-dangle': 0,
+    'func-names': 0,
+    'no-unused-expressions': 0,
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    'global-require': 0,
+    'implicit-arrow-linebreak': 0,
+    'function-paren-newline': 0,
+    'no-new': 0,
+    'arrow-parens': 0,
+    'operator-linebreak': 0,
+  },
 };
