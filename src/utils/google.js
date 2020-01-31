3eeb6a531d458a76e2add5a3be97348959cf2b46
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-31 11:05:57 +0100

    Added the server-api-url to the env example

---
+++ src/utils/google.js
@@ -0,0 +1,12 @@
+import passport from 'passport';
+import passportGoogle from 'passport-google-oauth';
+
+const GoogleStrategy = passportGoogle.OAuth2Strategy;
+
+const strategy = app => {
+  const strategyOptions = {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `${process.env.SERVER_API_URL}/auth/google/callback`,
+  };
+};
