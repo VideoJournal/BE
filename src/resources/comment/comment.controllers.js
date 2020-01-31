f6d841cafbc1900e9152d252999513240e972dce
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-31 09:03:07 +0100

    Created the getByVideoId controller

--- src/resources/comment/comment.controllers.js
+++ src/resources/comment/comment.controllers.js
@@ -3,4 +3,18 @@ import { Comment } from './comment.model';
 
 const controllers = crudControllers(Comment);
 
+const getByVideoID = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const comments = await Comment.find({ video: id })
+      .lean()
+      .exec();
+
+    res.status(200).json({ data: comments });
+  } catch (error) {
+    res.status(500).json({ error: `Could not get video's comments: ${error}` });
+  }
+};
+
 export default controllers;
